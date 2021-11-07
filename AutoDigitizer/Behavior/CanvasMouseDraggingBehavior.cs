1aa293e183dba36c246decb0e9f1ddbb3433119e
Kohei Konishi <m151109@ishikawa.kosen-ac.jp>
2021-11-08 00:01:03 +0900

    [ADD] Added a feature that user can draw a rectangle frame that specifies the chat area.

---
+++ AutoDigitizer/Behavior/CanvasMouseDraggingBehavior.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+#nullable enable
+namespace AutoDigitizer.Behavior {
+    internal class CanvasMouseDraggingBehavior {
+        public static readonly DependencyProperty StartPosProperty = DependencyProperty.RegisterAttached(
+            name: "StartPos",
+            propertyType: typeof(Point?),
+            ownerType: typeof(CanvasMouseDraggingBehavior),
+            defaultMetadata: new PropertyMetadata(null, MouseEventCallback)
+        );
+        public static Point GetStartPos(DependencyObject target) => (Point)target.GetValue(StartPosProperty);
+        public static void SetStartPos(DependencyObject target, Point? value) => target.SetValue(StartPosProperty, value);
+
+        public static readonly DependencyProperty EndPosProperty = DependencyProperty.RegisterAttached(
+            name: "EndPos",
+            propertyType: typeof(Point?),
+            ownerType: typeof(CanvasMouseDraggingBehavior),
+            defaultMetadata: new FrameworkPropertyMetadata(null, MouseEventCallback)
+        );
+        public static Point GetEndPos(DependencyObject target) => (Point)target.GetValue(EndPosProperty);
+        public static void SetEndPos(DependencyObject target, Point? value) => target.SetValue(EndPosProperty, value);
+
+        public static readonly DependencyProperty CursorPosProperty = DependencyProperty.RegisterAttached(
+            name: "CursorPos",
+            propertyType: typeof(Point?),
+            ownerType: typeof(CanvasMouseDraggingBehavior),
+            defaultMetadata: new FrameworkPropertyMetadata(null, MouseEventCallback)
+        );
+        public static Point GetCursorPos(DependencyObject target) => (Point)target.GetValue(CursorPosProperty);
+        public static void SetCursorPos(DependencyObject target, Point? value) => target.SetValue(CursorPosProperty, value);
+
+
+        private static void MouseClicked(object? sender, MouseEventArgs e) {
+            if (sender is not Canvas) return;
+            Canvas canvas = (Canvas)sender;
+
+            SetStartPos(canvas, e.GetPosition(canvas));
+        }
+
+        private static void MouseLeft(object? sender, MouseEventArgs e) {
+            if (sender is not Canvas) return;
+            Canvas canvas = (Canvas)sender;
+
+            SetEndPos(canvas, e.GetPosition(canvas));
+        }
+
+        private static void GetMousePosition(object? sender, MouseEventArgs e) {
+            if (sender is not Canvas) return;
+            Canvas canvas = (Canvas)sender;
+
+            if (e.LeftButton == MouseButtonState.Pressed) {
+                var pos = e.GetPosition(canvas);
+                SetCursorPos(canvas, e.GetPosition(canvas));
+            }
+        }
+
+        private static void MouseEventCallback(DependencyObject? sender, DependencyPropertyChangedEventArgs e) {
+            if (sender is not Canvas) return;
+            Canvas canvas = (Canvas)sender;
+
+            canvas.MouseDown += MouseClicked;
+            canvas.MouseLeave += MouseLeft;
+            canvas.MouseUp += MouseLeft;
+            canvas.MouseMove += GetMousePosition;
+        }
+    }
+}
--- AutoDigitizer/MainWindowViewModel.cs
+++ AutoDigitizer/MainWindowViewModel.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
+#nullable enable
 namespace AutoDigitizer {
     public class MainWindowViewModel: ViewModelBase {
         public Uri? FigurePath { get; private set; }
@@ -17,8 +19,43 @@ namespace AutoDigitizer {
                     return null;
             }
         }
-
         public FileOpening.OpenFileDialogCommand OFDCommand { get; private set; }
+
+        /// <summary>
+        /// The width of axis of the chart in display dimension.
+        /// </summary>
+        public Double AxisWidth { get; private set; } = 100;
+        /// <summary>
+        /// The height of axis of the chart in display dimension.
+        /// </summary>
+        public Double AxisHeight { get; private set; } = 100;
+        /// <summary>
+        /// The Left position of axis of the chart in display dimension.
+        /// </summary>
+        public Double AxisLeft { get; private set; } = 10;
+        /// <summary>
+        /// The Top position of axis of the chart in display dimension.
+        /// </summary>
+        public Double AxisTop { get; private set; } = 10;
+
+        private Point axisStartPos_;
+        public Point AxisStartPos {
+            get => this.axisStartPos_;
+            set {
+                this.axisStartPos_ = value;
+                this.calcAxisRect();
+            }
+        }
+
+        public Point axisEndPos_;
+        public Point AxisEndPos {
+            get => this.axisEndPos_;
+            set {
+                this.axisEndPos_ = value;
+                this.calcAxisRect();
+            }
+        }
+
         
         public MainWindowViewModel() {
             this.OFDCommand = new FileOpening.OpenFileDialogCommand();
@@ -32,5 +69,18 @@ namespace AutoDigitizer {
             this.RaisePropertyChanged(nameof(FigurePath));
             this.RaisePropertyChanged(nameof(ImageSource));
         }
+
+        private void calcAxisRect() {
+            this.AxisTop = Math.Min(AxisStartPos.Y, AxisEndPos.Y);
+            this.AxisLeft = Math.Min(AxisStartPos.X, AxisEndPos.X);
+            this.AxisWidth = Math.Abs(AxisStartPos.X - AxisEndPos.X);
+            this.AxisHeight = Math.Abs(AxisStartPos.Y - AxisEndPos.Y);
+
+            RaisePropertyChanged(nameof(AxisTop));
+            RaisePropertyChanged(nameof(AxisLeft));
+            RaisePropertyChanged(nameof(AxisWidth));
+            RaisePropertyChanged(nameof(AxisHeight));
+        }
+
     }
 }
